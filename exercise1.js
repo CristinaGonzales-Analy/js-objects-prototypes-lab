9bb13808273b60134e63b42c242455d961cc1114
Cristina Gonzales <2024161902@ub.edu.bz>
2026-02-03 18:44:52 -0600

    Complete Part 1: Function Syntax

---
+++ exercise1.js
@@ -0,0 +1,7 @@
+function calculateArea(length, width) {
+  return length * width;
+}
+
+console.log(calculateArea(5, 10));
+console.log(calculateArea(7, 3)); 
+console.log(calculateArea(4, 4));
---
+++ exercise2.js
@@ -0,0 +1,7 @@
+const calculatePerimeter = function (length, width) {
+  return 2 * length + 2 * width;
+};
+
+console.log(calculatePerimeter(5, 10)); 
+console.log(calculatePerimeter(7, 3));  
+console.log(calculatePerimeter(4, 4));  
